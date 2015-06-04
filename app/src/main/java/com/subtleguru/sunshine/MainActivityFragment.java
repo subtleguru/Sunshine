cfd12f1af3126e87ca220f8e842e496edd2b1030
Bobby Abraham <subtleguru@gmail.com>
2015-06-04 22:49:06 +0100

    list view added

--- app/src/main/java/com/subtleguru/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/subtleguru/sunshine/MainActivityFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -12,12 +18,38 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private ArrayAdapter<String> mForecastAdapter;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Create some dummy data for the ListView.  Here's a sample weekly forecast
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+
+        List<String> weekForecast = new ArrayList<>(Arrays.asList(data));
+
+        mForecastAdapter = new ArrayAdapter<>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast);
+
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView mListView = (ListView)rootView.findViewById(R.id.ListViewForecast);
+        mListView.setAdapter(mForecastAdapter);
+        return rootView;
     }
 }
